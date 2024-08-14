47b112da39ece71fccc8268635f94d67f0e3e616
Gilcecler Carneiro <gilcecler@gmail.com>
2024-08-14 10:01:34 -0300

    Projeto pronto passando no postman, sem seguranca e validacao

--- src/main/java/com/devsuperior/demo/controllers/EventController.java
+++ src/main/java/com/devsuperior/demo/controllers/EventController.java
@@ -3,8 +3,13 @@ package com.devsuperior.demo.controllers;
 import com.devsuperior.demo.services.EventService;
 import com.devsuperior.demo.dto.EventDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/events")
@@ -12,13 +17,26 @@ public class EventController {
 
     @Autowired
     private EventService service;
+
+    @GetMapping
+    public ResponseEntity<Page<EventDTO>> findAll(Pageable pageable) {
+        Page<EventDTO> list = service.findAllPaged(pageable);
+        return ResponseEntity.ok().body(list);
+    }
+
+    @PostMapping                         //@Valid
+    public ResponseEntity<EventDTO> insert(@RequestBody EventDTO dto) {
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
     @PutMapping(value = "/{id}")
     public ResponseEntity<EventDTO> update(@PathVariable Long id, @RequestBody EventDTO dto) {
         dto = service.update(id, dto);
         return ResponseEntity.ok().body(dto);
     }
-
-
 
 }
 
--- src/main/java/com/devsuperior/demo/services/EventService.java
+++ src/main/java/com/devsuperior/demo/services/EventService.java
@@ -8,6 +8,8 @@ import com.devsuperior.demo.entities.City;
 import com.devsuperior.demo.entities.Event;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,6 +21,21 @@ public class EventService {
 
     @Autowired
     private CityRepository cityRepository;
+
+    @Transactional(readOnly = true)
+    public Page<EventDTO> findAllPaged(Pageable pageable) {
+        Page<Event> list = repository.findAll(pageable);
+        return list.map(x -> new EventDTO(x));
+
+    }
+
+    @Transactional
+    public EventDTO insert(EventDTO dto) {
+        Event entity= new Event();
+        copyDtoToEntity(dto, entity);
+        entity = repository.save(entity);
+        return new EventDTO(entity);
+    }
 
     @Transactional
     public EventDTO update(Long id, EventDTO dto) {
